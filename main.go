48ec237fddd0367a0f4fe36a924c39e464b219fa
GuillemotClement <guillemot.clement@protonmail.com>
2026-06-05 21:55:55 +0200

    feat(cli): add args for durantion

--- main.go
+++ main.go
@@ -1,58 +1,70 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"strconv"
+
+	//	"strconv"
 	"time"
 
 	"github.com/gen2brain/beeep"
 )
 
-func getUserInput() (int, error) {
-	var durationStr string 
-	fmt.Println("Saisir une durée de travail en minute: ")
-	fmt.Scan(&durationStr)
+// func getUserInput() (int, error) {
+//	var durationStr string
+//	fmt.Println("Saisir une durée de travail en minute: ")
+//	fmt.Scan(&durationStr)
 
-	// conversion de la saisie 
-	duration, err := strconv.Atoi(durationStr)
-	if err != nil {
-		return 0, fmt.Errorf("la durée %d est invalide : %w", durationStr, err)
-	}
+// conversion de la saisie
+//	duration, err := strconv.Atoi(durationStr)
+//	if err != nil {
+//		return 0, fmt.Errorf("la durée %d est invalide : %w", durationStr, err)
+//	}
 
-	// check if valus is in the range 
-	if duration < 1 || duration > 60 {
-		return 0, fmt.Errorf("La durée en minute doit être entre 1 et 60: %d", duration)
-	}
+// check if valus is in the range
+//	if duration < 1 || duration > 60 {
+//		return 0, fmt.Errorf("La durée en minute doit être entre 1 et 60: %d", duration)
+//	}
 
-	return duration, nil
-}
+//	return duration, nil
+//}
 
-func startMinuterie(duration int){
-	durationInSecond := duration * 60  
+func startMinuterie(duration int) {
+	fmt.Println("Début de la session de travail. Have Fun !")
 
-	for i := durationInSecond; i > 0; i-- {
-		fmt.Printf("Temps restant: %d secondes\n", i)
+	durationinsecond := duration * 60
+
+	for i := durationinsecond; i > 0; i-- {
+		fmt.Printf("temps restant: %d secondes\n", i)
 		time.Sleep(1 * time.Second)
 	}
 
 	// send notification when period is end
 	err := beeep.Beep(beeep.DefaultFreq, beeep.DefaultDuration)
 	if err != nil {
-		fmt.Printf("Impossible de jour le super son de libération %v\n", err)
+		fmt.Printf("impossible de jour le super son de libération %v\n", err)
 	}
 	fmt.Println("Période de travail terminé")
 }
 
-func main(){
-	fmt.Println("Welcome to Pomogo, let's go start a new work session !")
-	duration, err := getUserInput()
+func main() {
+	// déclation des flags
+	// durationPtr := flag.String("duration", "", "Temps pour la session de travail")
+	// parse les arguments lance en ligne de commande au démarrage de l'outil
+	flag.Parse()
+
+	duration, err := strconv.Atoi(flag.Arg(0)) // on recupere le premier argument sans le flag
 	if err != nil {
-		fmt.Printf("erreur de saisie: %v", err)
+		fmt.Printf("saisie incorrect %q: %s\n", flag.Arg(0), err)
 		return
 	}
+	// durationStr := *durationPtr
 
+	//duration, err := strconv.Atoi(durationStr)
+	//if err != nil {
+	//fmt.Printf("saisie incorrect %q: %s", durationStr, err)
+	//return
+	//}
 	startMinuterie(duration)
 }
-
-
-
